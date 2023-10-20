2c57eb47baa2bb7e72857fab6ed093173fd328e5
rajwanraju <rajwan.diu@gmail.com>
2023-10-20 13:07:50 +0600

    missing pros added in car card

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -73,6 +73,7 @@ const CarCard = ({ car }: CarCardProps) => {
         <div className="car-card__btn-container">
           <CustomButtom
             title="View Details"
+            btnType="button"
             containerStyles="w-full py-[16px] rounded-full bg-primary-blue"
             textStyle="text-white text-[14px] leading-[17px] font-bold"
             rightIcon="/right-arrow.svg"
